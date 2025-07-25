68cf35dad13fb77d856f950c91de970673cec248
thivagar <thivagarradhakrishnan@gmail.com>
2025-07-25 18:41:42 +0530

    added cors

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://task-manager-api-frontend.vercel.app/login'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
